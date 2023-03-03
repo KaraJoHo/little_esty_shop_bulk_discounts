cd6582a25d8ce1a2a6ea3cc819395fb1db7a081f
KaraJH <karas.jones@yahoo.com>
2023-03-02 18:46:10 -0700

    feat: Create a new bulk discount with flash messages

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,10 +1,35 @@
 class BulkDiscountsController < ApplicationController 
+
   def index 
     @merchant = Merchant.find(params[:merchant_id])
   end
 
   def show 
     @discount = BulkDiscount.find(params[:id])
+  end
+
+  def new 
+    @discount = BulkDiscount.new
+  end
+
+  def create 
+    @merchant = Merchant.find(params[:merchant_id])
+    new_discount = @merchant.bulk_discounts.new(bulk_discount_params)
+    if new_discount.save
+      redirect_to merchant_bulk_discounts_path(@merchant)
+    else 
+      flash[:notice] = new_discount.errors.full_messages
+      redirect_to new_merchant_bulk_discount_path(@merchant)
+    end
+  end
+
+  private 
+  def bulk_discount_params 
+    params.require(:bulk_discount).permit(:name, :percentage_discount, :threshold)
+  end
+
+  def find_merchant 
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
 end
