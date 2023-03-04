b69a8e53272af97fab6740a0c06d07a43925e996
KaraJH <karas.jones@yahoo.com>
2023-03-04 14:53:55 -0700

    test: Total rev with discount for invoice item

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe Invoice, type: :model do
   describe "instance methods" do
     it "total_revenue" do
       @merchant1 = Merchant.create!(name: 'Hair Care')
+      @discount1 = @merchant1.bulk_discounts.create!(name: "Discount 1", percentage_discount: 10, threshold: 5)
       @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
       @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
       @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
@@ -22,6 +23,19 @@ RSpec.describe Invoice, type: :model do
       @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
 
       expect(@invoice_1.total_revenue).to eq(100)
+    end
+
+    it 'total_revenue_with_discount' do 
+      @merchant1 = Merchant.create!(name: 'Hair Care')
+      @discount1 = @merchant1.bulk_discounts.create!(name: "Discount 1", percentage_discount: 10, threshold: 5)
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
+      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+
+      expect(@invoice_1.total_revenue_with_discount).to eq(91)
     end
   end
 end
