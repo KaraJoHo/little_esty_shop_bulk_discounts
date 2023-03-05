d7fbae2626b49eb455ed0ab5dbea2a0495fa0b89
KaraJH <karas.jones@yahoo.com>
2023-03-05 13:13:23 -0700

    feat/fix: Add before action, and holidays to controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,12 +1,12 @@
 class BulkDiscountsController < ApplicationController 
+  before_action :find_merchant
 
   def index 
-    @merchant = Merchant.find(params[:merchant_id])
+    @upcoming_holidays = UpcomingHolidayInfo.new.next_3_holidays
   end
 
   def show 
     @discount = BulkDiscount.find(params[:id])
-    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def new 
@@ -14,7 +14,6 @@ class BulkDiscountsController < ApplicationController
   end
 
   def create 
-    @merchant = Merchant.find(params[:merchant_id])
     new_discount = @merchant.bulk_discounts.new(bulk_discount_params)
     if new_discount.save
       redirect_to merchant_bulk_discounts_path(@merchant)
@@ -25,12 +24,10 @@ class BulkDiscountsController < ApplicationController
   end
 
   def edit 
-    @merchant = Merchant.find(params[:merchant_id]) 
     @discount = BulkDiscount.find(params[:id])
   end
 
   def update 
-    @merchant = Merchant.find(params[:merchant_id]) 
     @discount = BulkDiscount.find(params[:id])
     
     if @discount.update(bulk_discount_params)
@@ -38,20 +35,13 @@ class BulkDiscountsController < ApplicationController
     else 
       flash[:notice] = @discount.errors.full_messages
       redirect_to edit_merchant_bulk_discount_path(@merchant, @discount)
-
-      # flash.now[:notice] = @discount.errors.full_messages
-      # render :edit
-      # expected: "/merchant/5840/bulk_discounts/2431/edit"
-      # got: "/merchant/5840/bulk_discounts/2431"
     end
-
   end
 
   def destroy 
-    merchant = Merchant.find(params[:merchant_id]) 
     discount = BulkDiscount.find(params[:id])
     discount.destroy 
-    redirect_to merchant_bulk_discounts_path(merchant)
+    redirect_to merchant_bulk_discounts_path(@merchant)
   end
 
   private 
